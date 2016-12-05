c7974d98aade2c1f902136a97f3555f54065f729
shoji-fujita <shoji.fujita.0819@gmail.com>
2016-12-06 00:53:26 +0900

    init

--- ViewFlipSample/ViewController.swift
+++ ViewFlipSample/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isFront = true
+    let view1 = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+    let view2 = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+    var flipFromLeft = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view1.backgroundColor = .red
+        view2.backgroundColor = .blue
+        view.addSubview(view1)
+        view.addSubview(view2)
+        view2.isHidden = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        flip(isFront: false)
     }
-
-
+    
+    func flip(isFront: Bool) {
+        if isFront {
+            let transitionOptions: UIViewAnimationOptions = [.transitionFlipFromLeft]
+            UIView.transition(with: view1, duration: 1.0, options: transitionOptions, animations: {
+                self.view1.isHidden = false
+            })
+            UIView.transition(with: view2, duration: 1.0, options: transitionOptions, animations: {
+                self.view2.isHidden = true
+            })
+        } else {
+            let transitionOptions: UIViewAnimationOptions = [.transitionFlipFromRight]
+            UIView.transition(with: view1, duration: 1.0, options: transitionOptions, animations: {
+                self.view1.isHidden = true
+            })
+            UIView.transition(with: view2, duration: 1.0, options: transitionOptions, animations: {
+                self.view2.isHidden = false
+            })
+        }
+        self.isFront = !isFront
+    }
+    
+    @IBAction func tappedButton(_ sender: UIButton) {
+        flip(isFront: isFront)
+    }
+    
 }
-
