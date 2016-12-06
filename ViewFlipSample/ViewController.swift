300ce19545f98d6b3cc28e1dd48574bbea38ff7f
shoji-fujita <shoji.fujita.0819@gmail.com>
2016-12-06 21:07:28 +0900

    bringSubviewで2つのViewの前後関係を変えながらFlipさせる方式に変更

--- ViewFlipSample/ViewController.swift
+++ ViewFlipSample/ViewController.swift
@@ -11,41 +11,33 @@ import UIKit
 class ViewController: UIViewController {
 
     var isFront = true
+    let view0 = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
     let view1 = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
     let view2 = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+    
     var flipFromLeft = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(view0)
         view1.backgroundColor = .red
         view2.backgroundColor = .blue
-        view.addSubview(view1)
-        view.addSubview(view2)
-        view2.isHidden = true
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        flip(isFront: false)
+        view0.addSubview(view1)
+        view0.addSubview(view2)
+        view0.bringSubview(toFront: view1)
     }
     
     func flip(isFront: Bool) {
         if isFront {
             let transitionOptions: UIViewAnimationOptions = [.transitionFlipFromLeft]
-            UIView.transition(with: view1, duration: 1.0, options: transitionOptions, animations: {
-                self.view1.isHidden = false
-            })
-            UIView.transition(with: view2, duration: 1.0, options: transitionOptions, animations: {
-                self.view2.isHidden = true
+            UIView.transition(with: view0, duration: 1.0, options: transitionOptions, animations: {
+                self.view0.bringSubview(toFront: self.view2)
             })
         } else {
             let transitionOptions: UIViewAnimationOptions = [.transitionFlipFromRight]
-            UIView.transition(with: view1, duration: 1.0, options: transitionOptions, animations: {
-                self.view1.isHidden = true
-            })
-            UIView.transition(with: view2, duration: 1.0, options: transitionOptions, animations: {
-                self.view2.isHidden = false
+            UIView.transition(with: view0, duration: 1.0, options: transitionOptions, animations: {
+                self.view0.bringSubview(toFront: self.view1)
             })
         }
         self.isFront = !isFront
